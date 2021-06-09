4d2da9f1eb28ac239f143f24d67013190bef69a7
[twasileva] <[twasileva@gmail.com]>
2021-06-09 22:25:56 +0300

    task-08 don't done

--- js/task-08.js
+++ js/task-08.js
@@ -1,15 +1,4 @@
-const block = {
-  width: 0,
-  height: 0,
-  backgroundColor: 0,
-  size() {
-    this.width = `30px`
-    this.height = `30px`
-    this.backgroundColor = colors[randomizer(colors.length)]
-
-  }
-
-}
+const block = ["width: 30px; height: 30px"]
 
 const colors = ["red", "blue", "green", "orange", "purple"];
 
@@ -17,35 +6,46 @@ const randomizer = max => {
   return Math.floor(Math.random() * max);
 };
 
-
-const controlsEl = document.getElementById('controls')
-console.log(controlsEl);
 const inputEl = document.querySelector('input')
-console.log(inputEl);
 const renderBtn = document.querySelector('button[data-action="render"]')
-console.log(renderBtn);
 const destroyBtn = document.querySelector('button[data-action="destroy"]')
-console.log(destroyBtn);
+const boxesEl = document.querySelector('#boxes')
 
-const boxesEl = document.getElementById('boxes')
-console.log(boxesEl);
+const form = document.createElement('div')
+boxesEl.append(form)
 
 
+const setInputValue = inputEl.addEventListener('input', e => {
+  const inputValue = Number(e.currentTarget.value);
+  console.log(inputValue);
+
+})
 
 renderBtn.addEventListener('click', () => {
   createBoxes()
+
 })
 
 const createBoxes = (amount) => {
 
+  for (let i = 0; i <= amount; i += 1) {
 
+    form.style.cssText = block[randomizer(block.length)]
+    form.style.backgroundColor = colors[randomizer(colors.length)]
 
-  // const block = document.createElement('div')
-  // block.style.backgroundColor = colors[randomizer(colors.length)]
-  // block.style.width = '30px'
-  // block.style.height = '30px'
-
-  boxesEl.append(block.size(30, 30, 30))
+  }
+  return form
 }
+console.log(createBoxes(setInputValue));
 
-console.log(createBoxes(3));
+// алгоритм решения задачи
+
+// задать начальные данные блока в массиве
+// сделать рандомную функцию для выбора цвета
+// массив цветов
+// получить доступ ко всем дом-узлам: инпут, кнопки, див
+// получить значение из инпута
+// повесить слушателя событий на кнопку Создать, куда передать функцию createBoxes
+// написать ф-цию createBoxes
+// повесить слушателя событий на кнопку Очистить
+
